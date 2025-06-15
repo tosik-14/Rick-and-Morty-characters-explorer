7d61a37a4f8b8be13275aa83224f005d2be26abb
tosik-14 <anton.mikhailouski@gmail.com>
2025-06-15 21:39:16 +0300

    debuged api request

--- src/services/api/getFilteredCharacters.ts
+++ src/services/api/getFilteredCharacters.ts
@@ -2,8 +2,8 @@ import { API_URL } from '@env';
 
 export async function getFilteredCharacters(page: number = 1, params: Record<string, string>) {
     const paramsString = new URLSearchParams(params).toString();
-    console.log("PARAMS", paramsString);
-    const response = await fetch(`${API_URL}/character/?${page}&${paramsString}`);
+    console.log("PAGE AND PARAMS", page, paramsString);
+    const response = await fetch(`${API_URL}/character/?page=${page}&${paramsString}`);
 
     if(!response.ok){
         throw new Error(`Error loading character:, ${response.status}`);
