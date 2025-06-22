ed8d12a2c7f17ecc3c70f8435004e2d50c145bfc
tosik-14 <anton.mikhailouski@gmail.com>
2025-06-22 22:00:11 +0300

    feat: interface added

---
+++ src/features/character/characterDetailsModel/types.ts
@@ -0,0 +1,11 @@
+import {characterStatusEnum} from "@/src/entities/character/characterStatusEnum";
+import {characterGenderEnum} from "@/src/entities/character/characterGenderEnum";
+
+export interface CharacterDetailsModel {
+    id: number;
+    name: string;
+    status: characterStatusEnum;
+    species: string;
+    gender: characterGenderEnum;
+    image: string;
+}
